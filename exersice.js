d60dcfcad7dc7b2bf1240e06f9c5e73f6752c54f
Nada Hassan <167430924+nadahassan15@users.noreply.github.com>
2024-05-08 13:51:47 +0300

    Create exersice.js

---
+++ exersice.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+document.getElementById("exerciseSelected").addEventListener("change", function() {
+    if (this.value !== "Select") {
+        modal.style.display = "block";
+    }
+});
+span.onclick = function() {
+    modal.style.display = "none";
+};
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }}
+;
